b05ce2a2dcf03fa1a876cc957ca3cea8415eecdb
Timofey Rytikov <7timka@gmail.com>
2015-06-08 20:55:05 -0700

    init

---
+++ app/src/main/java/co/rytikov/spotifystreamer/ArtistActivityFragment.java
@@ -0,0 +1,23 @@
+package co.rytikov.spotifystreamer;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ArtistActivityFragment extends Fragment {
+
+    public ArtistActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_artist, container, false);
+    }
+}
